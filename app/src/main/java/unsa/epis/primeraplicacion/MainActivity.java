2dfa6bb380911d590fd285da2664af72a59620d3
Elmer Davila <40619853+elmerdavila@users.noreply.github.com>
2020-09-24 22:12:29 -0500

    Initial commit

---
+++ app/src/main/java/unsa/epis/primeraplicacion/MainActivity.java
@@ -0,0 +1,14 @@
+package unsa.epis.primeraplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
